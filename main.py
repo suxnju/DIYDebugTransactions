d127ece044873b8dfd0127433e72267084c91511
苏醒 <suxnju@smail.nju.edu.cn>
2021-03-15 15:56:11 +0800

    sync

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from Structure.EVM import EVM,EVM_stack,EVM_memory,EVM_storage
 import os
 import json
 import logging
+import pandas as pd
+import math
+
 from Structure.Constant import OPCODE_TO_INSTR
 from Structure.Transaction import Transaction
 
+from collections import Counter
 def load_opcodes(file_path:str) -> 'Dict':
     opcodes = {}
     with open(file_path,"r",encoding="utf-8") as f:
@@ -20,7 +24,6 @@ def load_opcodes(file_path:str) -> 'Dict':
     return opcodes
 
 def execute_init():
-    f = open("./log/running/init.log","w",encoding="utf-8")
     opcodes = load_opcodes("./data/init.disassemble")
     
     tx_0 = Transaction(
@@ -40,9 +43,38 @@ def execute_init():
     while True:
         opcode = opcodes[evm.pc][0]
         args = opcodes[evm.pc][1]
-        
-        f.write("stack:[%s]\nmemory:%s\nstorage:%s\n%s\n\n"%(str(evm.Stack),str(evm.Memory),str(evm.Storage),"="*10+hex(evm.pc)+"_"+str(evm.pc)+":"+str(opcodes[evm.pc])+"="*10))
-        f.flush()
+
+        if opcode in ["RETURN","STOP","REVERT"]:
+            break
+        evm.pc += 1
+        if args is not None:
+            args_bytes = int(opcode.lstrip("PUSH"))
+            evm.pc += args_bytes
+
+        evm.args = args
+        eval_function = "evm.%s()"%opcode        
+        eval(eval_function)
+
+    return evm.Storage, {"read":evm.readStorage,"write":evm.writeStorage}
+
+def execute_tx(storage:'EVM_storage',transaction:'Transaction',opcodes:'Dict',DEBUG_Point:int=0x0,verbose:bool=False):
+    evm = EVM(
+        Stack=EVM_stack(),
+        Memory=EVM_memory(),
+        Storage=storage,
+        Transaction=transaction
+    )
+    tx_hash = transaction.get("tx_hash")
+    
+    if verbose:
+        f = open("./log/running/%s.log"%tx_hash,"w",encoding="utf-8")
+
+    while True:
+        opcode = opcodes[evm.pc][0]
+        args = opcodes[evm.pc][1]
+
+        if verbose:
+            f.write("stack:[%s]\nmemory:%s\nstorage:%s\n%s\n\n"%(str(evm.Stack),str(evm.Memory),str(evm.Storage),"="*10+hex(evm.pc)+"_"+str(evm.pc)+":"+str(opcodes[evm.pc])+"="*10))
 
         if opcode in ["RETURN","STOP","REVERT"]:
             if opcode == "REVERT":
@@ -57,56 +89,52 @@ def execute_init():
         eval_function = "evm.%s()"%opcode        
         eval(eval_function)
 
-    f.close()
+    if verbose:
+        f.close()
+        with open("./log/storage_readwrite/%s.json"%tx_hash,"w",encoding="utf-8") as f:
+            json.dump({"read":[v.rjust(64,"0") for v in evm.readStorage],"write":[v.rjust(64,"0") for v in evm.writeStorage]},f,indent='\t')
 
-    with open("./log/storage_readwrite/storage_init.json","w",encoding="utf-8") as f:
-        json.dump({"read":evm.readStorage,"write":evm.writeStorage},f,indent='\t')
+        with open("./log/storage/%s.json"%tx_hash,"w",encoding="utf-8") as f:
+            f.write(str(evm.Storage))
+
+    return evm.Storage, {"read":evm.readStorage,"write":evm.writeStorage}
+
+def load_group(data_file="./data/game_group.csv") -> 'Dict':
+    exec_txs = pd.read_csv(data_file,sep=',')
+    group = {}
+    for row, exec_tx in exec_txs.iterrows():
+        if exec_tx['data'] not in group:
+            group[exec_tx['data']] = [exec_tx['transaction_hash']]
+        else:
+            group[exec_tx['data']].append(exec_tx['transaction_hash'])
     
-    return evm.Storage
+    return group
 
-def execute_tx(storage:'EVM_storage',transaction:'Transaction',opcodes:'Dict',DEBUG_Point:int=0x0):
-    evm = EVM(
-        Stack=EVM_stack(),
-        Memory=EVM_memory(),
-        Storage=storage,
-        Transaction=transaction
-    )
-    tx_hash = transaction.get("tx_hash")
+def load_data(data_file="./data/game_txs.csv") -> 'Transaction':
+    exec_txs = pd.read_csv(data_file,sep=',').sort_values(by=['block_timestamp','transaction_index'],ascending=(True,True))
+    for row, exec_tx in exec_txs.iterrows():
+        if exec_tx['hash'] == "0x79a09f9843b1248b192ea05f36b60686d3ca5bbee7020f7431aed669131516c7": # init has executed
+            continue
+        if exec_tx['receipt_status'] == 0: # ignore failed transactions
+            continue
+        print("\r%d"%row,end='')
+        yield Transaction(
+            tx_hash=exec_tx['hash'],
+            msg_caller=exec_tx['from_address'],
+            msg_value=int(exec_tx['value']),
+            msg_input=exec_tx['input'],
+            timestamp=exec_tx['block_timestamp']
+        )
 
-    logging.info("="*20+"Running Transaction %s"%tx_hash+"="*20)
+def check_common_slot(groupid:int,txs:'list',storage_modified:'Dict') -> 'bool':
+    commons = set()
+    for i, tx_hash in enumerate(txs):
+        if i == 0:
+            commons = commons | set(storage_modified[tx_hash]['read'] + storage_modified[tx_hash]['write'])
+        else:
+            commons = commons & set(storage_modified[tx_hash]['read'] + storage_modified[tx_hash]['write'])
 
-    f = open("./log/running/%s.log"%tx_hash,"w",encoding="utf-8")
-    
-    while True:
-        opcode = opcodes[evm.pc][0]
-        args = opcodes[evm.pc][1]
-        
-        if evm.pc == DEBUG_Point and DEBUG_Point is not 0x0:
-            print(end='')
-
-        f.write("stack:[%s]\nmemory:%s\nstorage:%s\n%s\n\n"%(str(evm.Stack),str(evm.Memory),str(evm.Storage),"="*10+hex(evm.pc)+"_"+str(evm.pc)+":"+str(opcodes[evm.pc])+"="*10))
-        f.flush()
-
-        if opcode in ["RETURN","STOP","REVERT"]:
-            break
-        evm.pc += 1
-        if args is not None:
-            args_bytes = int(opcode.lstrip("PUSH"))
-            evm.pc += args_bytes
-
-        evm.args = args
-        eval_function = "evm.%s()"%opcode        
-        eval(eval_function)
-
-    f.close()
-
-    with open("./log/storage_readwrite/%s.json"%tx_hash,"w",encoding="utf-8") as f:
-        json.dump({"read":evm.readStorage,"write":evm.writeStorage},f,indent='\t')
-
-    with open("./log/storage/%s.json"%tx_hash,"w",encoding="utf-8") as f:
-        f.write(str(evm.Storage))
-
-    return evm.Storage
+    return commons
 
 def mk_dirs():
     if not os.path.exists("./log/storage"):
@@ -118,88 +146,52 @@ def mk_dirs():
     if not os.path.exists("./log/running"):
         os.makedirs("./log/running")
 
-if __name__ == "__main__":
-    mk_dirs()
-    logging.basicConfig(
-        filename="./log/init.log",
-        filemode="a",
-        level=logging.INFO
-    )
-    storage = execute_init()
+def replay_transactions():
+    storage, storage_init = execute_init()
     
+    storage_modified = {
+        "init":storage_init
+    }
     opcodes = load_opcodes("./data/game.disassemble")
 
-    # Create Gameid 01
-    tx_1 = Transaction(
-        tx_hash="0xaa8fcdb649889f7f1b63c37f34902650ffb2faedea4b3c6b6630f251f8aedbd8",
-        msg_caller="0x5dc12131e65b8f395ab11a2c4e6af717e1b179ba",
-        msg_value=50000000000000000,
-        msg_input="0xfe1f6a0bd579d4fe1e90a03d545e3d8c01dfc19c2ae3b26ad26ba994a1dec89a435a3dc00000000000000000000000000000000000000000000000000000000000000000",
-        timestamp="2018-08-19 15:05:16 UTC"
-    )
+    for tx in load_data():
+        if tx.get("tx_hash") == "0x8b20e7edcdb9a58a9d7b5fe08795ba0ff11bb8b4e0e1ebffeba03e2e50075681":
+            verbose = True
+        else:
+            verbose = False
+        storage, storage_read_write = execute_tx(
+            storage=storage,
+            transaction=tx,
+            opcodes=opcodes,
+            verbose=verbose
+        )
+        storage_modified[tx.get("tx_hash")] = storage_read_write
+       
+    with open("result.json","w",encoding="utf-8") as f:
+        json.dump(storage_modified,f,indent='\t')
 
-    storage_after_1 = execute_tx(
-        storage=storage,
-        transaction=tx_1,
-        opcodes=opcodes
-    )
-
-    # Join Gameid 01
-    tx_2 = Transaction(
-        tx_hash="0xabd8b650c2cdab9699bcfd52f3b24b991dc4d2abf7b24f8618bf8c1801f7f8ce",
-        msg_caller="0xa70e846b87366ac57cf78fb2222e1ca404ba5406",
-        msg_value=50000000000000000,
-        msg_input="0xca6649c50000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000a",
-        timestamp="2018-08-19 15:09:02 UTC"
-    )
-
-    storage_after_2 = execute_tx(
-        storage=storage_after_1,
-        transaction=tx_2,
-        opcodes=opcodes
-    )
-
-    # Reveal & Close Gameid 01
-    tx_3 = Transaction(
-        tx_hash="0x4e7d11c4a1e726aac1e09bb7306c226337fad8e91907302f58f21123ef3f0713",
-        msg_caller="0x5dc12131e65b8f395ab11a2c4e6af717e1b179ba",
-        msg_value=0,
-        msg_input="0x9a42f3aa0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000001e3e0db1bcb49b1666382383bb70ade300f1668972fd67a628673ceb1a402bcda3",
-        timestamp="2018-08-19 15:10:09 UTC"
-    )
-
-    storage_after_3 = execute_tx(
-        storage=storage_after_2,
-        transaction=tx_3,
-        opcodes=opcodes
-    )
-
-    # Create Gameid 02
-    tx_4 = Transaction(
-        tx_hash="0x6694d7e86778e0e92e57d463009d8ba184b83cbf249df18c9ce606e0f04a6671",
-        msg_caller="0xa70e846b87366ac57cf78fb2222e1ca404ba5406",
-        msg_value=50000000000000000,
-        msg_input="0xfe1f6a0b0a756ddfd731b3648078c31009bb4c752a2547fbdf70f16f7ec7e0112671f9020000000000000000000000000000000000000000000000000000000000000000",
-        timestamp="2018-08-20 03:02:49 UTC"
-    )
-    storage_after_4 = execute_tx(
-        storage=storage_after_3,
-        transaction=tx_4,
-        opcodes=opcodes
-    )
+def verfied_group():
+    with open("result.json","r",encoding="utf-8") as f:
+        storage_modified = json.load(f)
     
-    # Close Gameid 02
-    tx_5 = Transaction(
-        tx_hash="0x2c14559b934f471b83cab660b1227b69422b9a2c57a2ec6f229399240aa3f924",
-        msg_caller="0xa70e846b87366ac57cf78fb2222e1ca404ba5406",
-        msg_value=0,
-        msg_input="0x0aebeb4e0000000000000000000000000000000000000000000000000000000000000002",
-        timestamp="2018-08-20 03:36:14 UTC"
-    )
-    storage_after_5 = execute_tx(
-        storage=storage_after_4,
-        transaction=tx_5,
-        opcodes=opcodes,
-        DEBUG_Point=0x32
-    )
-    print()
+    group = load_group()
+    commons = {}
+    id_list = list(group.keys())
+    for groupid in id_list:
+        common_slots = check_common_slot(groupid,group[groupid],storage_modified)
+        commons[groupid] = common_slots
+
+    for i in range(0,len(id_list)):
+        gameid = id_list[i]
+        for j in range(i+1,len(id_list)):
+            o_gameid = id_list[j]
+
+            commons[gameid] = commons[gameid] - commons[o_gameid]
+        commons[gameid] = list(commons[gameid])
+
+    with open("commons.json","w",encoding="utf-8") as f:
+        json.dump(commons,f,indent='\t')
+
+if __name__ == "__main__":
+    mk_dirs()
+    verfied_group()
---
+++ run_scample.py
@@ -0,0 +1,205 @@
+from Structure.EVM import EVM,EVM_stack,EVM_memory,EVM_storage
+
+import os
+import json
+import logging
+from Structure.Constant import OPCODE_TO_INSTR
+from Structure.Transaction import Transaction
+
+def load_opcodes(file_path:str) -> 'Dict':
+    opcodes = {}
+    with open(file_path,"r",encoding="utf-8") as f:
+        for line in f.readlines():
+            line = line.strip().split(" ")
+            opcode = line[1]
+            if len(line) > 2:
+                args = int(line[-1],16)
+            else:
+                args = None
+            opcodes[int(line[0],16)] = (opcode,args)
+    return opcodes
+
+def execute_init():
+    f = open("./log/running/init.log","w",encoding="utf-8")
+    opcodes = load_opcodes("./data/init.disassemble")
+    
+    tx_0 = Transaction(
+        tx_hash="0x79a09f9843b1248b192ea05f36b60686d3ca5bbee7020f7431aed669131516c7",
+        msg_caller="0x5dc12131e65b8f395ab11a2c4e6af717e1b179ba",
+        msg_value=0,
+        msg_input="0", # mask input
+        timestamp="2018-08-19 14:50:21 UTC"
+    )
+
+    evm = EVM(
+        Stack=EVM_stack(),
+        Memory=EVM_memory(),
+        Storage=EVM_storage({}),
+        Transaction=tx_0
+    )
+    while True:
+        opcode = opcodes[evm.pc][0]
+        args = opcodes[evm.pc][1]
+        
+        f.write("stack:[%s]\nmemory:%s\nstorage:%s\n%s\n\n"%(str(evm.Stack),str(evm.Memory),str(evm.Storage),"="*10+hex(evm.pc)+"_"+str(evm.pc)+":"+str(opcodes[evm.pc])+"="*10))
+        f.flush()
+
+        if opcode in ["RETURN","STOP","REVERT"]:
+            if opcode == "REVERT":
+                print()
+            break
+        evm.pc += 1
+        if args is not None:
+            args_bytes = int(opcode.lstrip("PUSH"))
+            evm.pc += args_bytes
+
+        evm.args = args
+        eval_function = "evm.%s()"%opcode        
+        eval(eval_function)
+
+    f.close()
+
+    with open("./log/storage_readwrite/storage_init.json","w",encoding="utf-8") as f:
+        json.dump({"read":evm.readStorage,"write":evm.writeStorage},f,indent='\t')
+    
+    return evm.Storage
+
+def execute_tx(storage:'EVM_storage',transaction:'Transaction',opcodes:'Dict',DEBUG_Point:int=0x0):
+    evm = EVM(
+        Stack=EVM_stack(),
+        Memory=EVM_memory(),
+        Storage=storage,
+        Transaction=transaction
+    )
+    tx_hash = transaction.get("tx_hash")
+
+    logging.info("="*20+"Running Transaction %s"%tx_hash+"="*20)
+
+    f = open("./log/running/%s.log"%tx_hash,"w",encoding="utf-8")
+    
+    while True:
+        opcode = opcodes[evm.pc][0]
+        args = opcodes[evm.pc][1]
+        
+        if evm.pc == DEBUG_Point and DEBUG_Point is not 0x0:
+            print(end='')
+
+        f.write("stack:[%s]\nmemory:%s\nstorage:%s\n%s\n\n"%(str(evm.Stack),str(evm.Memory),str(evm.Storage),"="*10+hex(evm.pc)+"_"+str(evm.pc)+":"+str(opcodes[evm.pc])+"="*10))
+        f.flush()
+
+        if opcode in ["RETURN","STOP","REVERT"]:
+            break
+        evm.pc += 1
+        if args is not None:
+            args_bytes = int(opcode.lstrip("PUSH"))
+            evm.pc += args_bytes
+
+        evm.args = args
+        eval_function = "evm.%s()"%opcode        
+        eval(eval_function)
+
+    f.close()
+
+    with open("./log/storage_readwrite/%s.json"%tx_hash,"w",encoding="utf-8") as f:
+        json.dump({"read":evm.readStorage,"write":evm.writeStorage},f,indent='\t')
+
+    with open("./log/storage/%s.json"%tx_hash,"w",encoding="utf-8") as f:
+        f.write(str(evm.Storage))
+
+    return evm.Storage
+
+def mk_dirs():
+    if not os.path.exists("./log/storage"):
+        os.makedirs("./log/storage")
+    
+    if not os.path.exists("./log/storage_readwrite"):
+        os.makedirs("./log/storage_readwrite")
+    
+    if not os.path.exists("./log/running"):
+        os.makedirs("./log/running")
+
+if __name__ == "__main__":
+    mk_dirs()
+    logging.basicConfig(
+        filename="./log/init.log",
+        filemode="a",
+        level=logging.INFO
+    )
+    storage = execute_init()
+    
+    opcodes = load_opcodes("./data/game.disassemble")
+
+    # Create Gameid 01
+    tx_1 = Transaction(
+        tx_hash="0xaa8fcdb649889f7f1b63c37f34902650ffb2faedea4b3c6b6630f251f8aedbd8",
+        msg_caller="0x5dc12131e65b8f395ab11a2c4e6af717e1b179ba",
+        msg_value=50000000000000000,
+        msg_input="0xfe1f6a0bd579d4fe1e90a03d545e3d8c01dfc19c2ae3b26ad26ba994a1dec89a435a3dc00000000000000000000000000000000000000000000000000000000000000000",
+        timestamp="2018-08-19 15:05:16 UTC"
+    )
+
+    storage_after_1 = execute_tx(
+        storage=storage,
+        transaction=tx_1,
+        opcodes=opcodes
+    )
+
+    # Join Gameid 01
+    tx_2 = Transaction(
+        tx_hash="0xabd8b650c2cdab9699bcfd52f3b24b991dc4d2abf7b24f8618bf8c1801f7f8ce",
+        msg_caller="0xa70e846b87366ac57cf78fb2222e1ca404ba5406",
+        msg_value=50000000000000000,
+        msg_input="0xca6649c50000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000a",
+        timestamp="2018-08-19 15:09:02 UTC"
+    )
+
+    storage_after_2 = execute_tx(
+        storage=storage_after_1,
+        transaction=tx_2,
+        opcodes=opcodes
+    )
+
+    # Reveal & Close Gameid 01
+    tx_3 = Transaction(
+        tx_hash="0x4e7d11c4a1e726aac1e09bb7306c226337fad8e91907302f58f21123ef3f0713",
+        msg_caller="0x5dc12131e65b8f395ab11a2c4e6af717e1b179ba",
+        msg_value=0,
+        msg_input="0x9a42f3aa0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000001e3e0db1bcb49b1666382383bb70ade300f1668972fd67a628673ceb1a402bcda3",
+        timestamp="2018-08-19 15:10:09 UTC"
+    )
+
+    storage_after_3 = execute_tx(
+        storage=storage_after_2,
+        transaction=tx_3,
+        opcodes=opcodes
+    )
+
+    # Create Gameid 02
+    tx_4 = Transaction(
+        tx_hash="0x6694d7e86778e0e92e57d463009d8ba184b83cbf249df18c9ce606e0f04a6671",
+        msg_caller="0xa70e846b87366ac57cf78fb2222e1ca404ba5406",
+        msg_value=50000000000000000,
+        msg_input="0xfe1f6a0b0a756ddfd731b3648078c31009bb4c752a2547fbdf70f16f7ec7e0112671f9020000000000000000000000000000000000000000000000000000000000000000",
+        timestamp="2018-08-20 03:02:49 UTC"
+    )
+    storage_after_4 = execute_tx(
+        storage=storage_after_3,
+        transaction=tx_4,
+        opcodes=opcodes
+    )
+    
+    # Close Gameid 02
+    tx_5 = Transaction(
+        tx_hash="0x2c14559b934f471b83cab660b1227b69422b9a2c57a2ec6f229399240aa3f924",
+        msg_caller="0xa70e846b87366ac57cf78fb2222e1ca404ba5406",
+        msg_value=0,
+        msg_input="0x0aebeb4e0000000000000000000000000000000000000000000000000000000000000002",
+        timestamp="2018-08-20 03:36:14 UTC"
+    )
+    storage_after_5 = execute_tx(
+        storage=storage_after_4,
+        transaction=tx_5,
+        opcodes=opcodes,
+        DEBUG_Point=0x32
+    )
+    print()
